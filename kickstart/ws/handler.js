e653ede84b98682d78b1c0b2f91e8e0192d7b7d9
Graham <graham.crosmarie@atchik-services.com>
2018-04-19 20:15:31 +0200

    Add ws listener

---
+++ kickstart/ws/handler.js
@@ -0,0 +1,24 @@
+const debug = require('debug')('ws-handler');
+const users = require('../lib/user')
+const events= require('../lib/events')
+
+function handleSocket(socket) {
+  debug('User connected:', socket.id)
+  users.create(socket.id, true).then(user => {
+  	debug('User saved and is online:', user.uid)
+  	
+	  socket.on('disconnect', () => {
+      users.disconnect(user).then(() => {
+        debug('User disconnected:', user.uid)
+      })
+	  })
+	  
+	  socket.on('track', event => {
+	  	debug('Event arrived:', event.channel, user.uid)
+	  	events.create(user, event.channel).then(() => debug('Event saved:', event.channel, user.uid))
+	  })
+  })
+  
+}
+
+module.exports = handleSocket
